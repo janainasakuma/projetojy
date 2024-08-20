f28b7ac84039b376dd6695f093c25766f54d1419
janainasakuma <sakumajanaina@gmail.com>
2024-08-20 07:57:14 -0300

    first commit

---
+++ bilheteria/src/componentes/header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+    return (
+        <Header>
+            <div className='Header center'>
+                <picture>
+                    <img src='./assets/logo.png' alt='logo otica'/>
+                </picture>
+
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='#'>Produtos</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>Sobre</a>
+                        </li>
+
+                        <li>
+                            <a href='#'>Contatos</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </Header>
+    )
+}
+
+export default Header;
