7ca4dd0a43d9740cbd50d8883039d21af4842932
janainasakuma <sakumajanaina@gmail.com>
2024-08-27 08:49:31 -0300

    27-08-24

--- bilheteria/src/App.js
+++ bilheteria/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './componentes/Header';
+import SecaoCapa from './componentes/SecaoCapa';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SecaoCapa/>
     </div>
   );
 }
---
+++ bilheteria/src/componentes/SecaoCapa/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+const SecaoCapa = () => {
+    return (
+        <section className='secao-capa'>
+            <div className='capa center'>
+                <div className='texto-capa'>
+                    <p>Preços Baixos em</p>
+                    <h1>Oculos de grau e de sol</h1>
+                    <p>Você so encontra aqui</p>
+                </div>
+            </div>
+        </section>  
+    )
+}
+
+export default SecaoCapa;
