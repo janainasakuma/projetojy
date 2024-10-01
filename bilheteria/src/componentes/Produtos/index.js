e5f491de60a654d495eaae4c51b7e40f1072872c
janainasakuma <sakumajanaina@gmail.com>
2024-10-01 08:53:03 -0300

    01/10

---
+++ bilheteria/src/componentes/Produtos/index.js
@@ -0,0 +1,66 @@
+import React from "react";
+import './style.css';
+
+const Produtos = () => {
+    return (
+        <section id="produtos">
+            <div className="secao-produtos center">
+            <h2 className="titulos">NOSSOS PRODUTOS</h2>
+            <p>
+                Trabalhamos com óculos de grau, óculos de sol, 
+                lentes transitions nos modelos masculinos, femininos e infantis.
+            </p>
+
+            <p>
+                Todos os nossos preços são acessíveis e contam com a 
+                melhor qualidade do mercado.
+            </p>
+            <div className="oculos-img">
+                <div className="card-oculos">
+                    <h3>Óculos de Grau</h3>
+                    <picture>
+                        <img src="./assets/oculos01.png" alt="Óculos de grau"></img>
+                    </picture>
+                    <p>R$ 550,00</p>
+                </div>
+
+                <div className="card-oculos">
+                    <h3>Óculos Transition</h3>
+                    <picture>
+                        <img src="./assets/oculos02.png" alt="Óculos de grau"></img>
+                    </picture>
+                    <p>R$ 1.550,00</p>
+                </div>
+
+                <div className="card-oculos">
+                    <h3>Óculos de Sol</h3>
+                    <picture>
+                        <img src="./assets/oculos03.png" alt="Óculos de grau"></img>
+                    </picture>
+                    <p>R$ 750,00</p>
+                </div>
+
+                <div className="card-oculos">
+                    <h3>Óculos Infantil</h3>
+                    <picture>
+                        <img src="./assets/oculos04.png" alt="Óculos de grau"></img>
+                    </picture>
+                    <p>R$ 250,00</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="lista-oculos center">
+                <p>Todos os nossos produtos incluem:</p>
+                <ul>
+                    <li>Garantia</li>
+                    <li>Proteção</li>
+                    <li>Descontos</li>
+                    <li>Durabilidade</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Produtos;
