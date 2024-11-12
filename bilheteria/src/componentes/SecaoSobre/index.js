c4fbc14fcc33733e79fb494e22dbfa0c11cf054d
janainasakuma <sakumajanaina@gmail.com>
2024-11-12 08:43:46 -0300

    12/11 segunda vez

--- bilheteria/src/componentes/SecaoSobre/index.js
+++ bilheteria/src/componentes/SecaoSobre/index.js
@@ -7,14 +7,14 @@ const SecaoSobre = () => {
         <div className='sobre center'>
             <h2>SOBRE NÓS</h2>
                 <p>fundada em 2001, em caraguatatuba - Sâo Paulo, a otica iniciou ...</p>
-        </div>
+        
 
         <div className='elementos-sobre'>
             <picture>
                 <img src='./assets/loja.png' 
                 alt='Nossa loja'/>
             </picture>
-        </div>
+        
 
         <div className='sobre-elementos primeiro-sobre'>
             <h4>NOSSAS FILIAIS</h4>
@@ -28,11 +28,14 @@ const SecaoSobre = () => {
             <p>
                 Nossa equipe é treinada para te atender da melhor forma possível
             </p>
-        </div>
-
+            </div>
+        
         <picture>
             <img src='./assets/atendimento.png' alt='Atendimento'/>
         </picture>
+        
+        </div>
+        </div>
     </section>
     )
 }
